47ca4390c67d5382400fd4e02a5640a66c4b470e
Rishabh Tatiraju <tatiraju.rishabh@gmail.com>
2021-11-21 14:24:27 -0500

    indentation

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from flask_cors import CORS
 import bl_mock as bl
 
 app = Flask(__name__, static_folder='static', static_url_path='')
-CORS(app) 
+CORS(app)
+
 
 @app.route('/')
 def root():
     return "This is the root server route and does nothing."
+
 
 @app.route('/heath_check')
 def health_check():
@@ -16,13 +18,15 @@ def health_check():
 
 @app.route('/things')
 def get_things():
-    result = bl.get_things();
+    result = bl.get_things()
     return jsonify(result), result['code']
+
 
 @app.route('/things/<int:thing_id>/services')
 def get_things():
-    result = bl.get_things();
+    result = bl.get_things()
     return jsonify(result), result['code']
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=2080)
