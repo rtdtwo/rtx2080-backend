235a951b072538b04d649a1ae3f96388ece082f5
Rishabh Tatiraju <tatiraju.rishabh@gmail.com>
2021-11-21 12:44:55 -0500

    Added first API

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__, static_folder='static', static_url_path='')
+
+
+@app.route('/heath_check')
+def health_check():
+    return jsonify({'code': 200, 'msg': 'Server running!'}, 200)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=2080)
