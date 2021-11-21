8e193dc6ef5caa9e83d71bb5e65fc77557dd22ba
treadwill <td_1>
2021-11-22 00:50:13 +0530

    added-CORS

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import bl_mock as bl
 
 app = Flask(__name__, static_folder='static', static_url_path='')
+CORS(app) 
 
 @app.route('/')
 def root():
@@ -17,6 +19,10 @@ def get_things():
     result = bl.get_things();
     return jsonify(result), result['code']
 
+@app.route('/things/<int:thing_id>/services')
+def get_things():
+    result = bl.get_things();
+    return jsonify(result), result['code']
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=2080)
