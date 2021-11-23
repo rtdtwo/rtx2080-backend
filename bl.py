4c9549553a5b536a75cb044d370586b7d8061301
treadwill <td_1>
2021-11-23 10:05:32 +0530

    services

--- bl.py
+++ bl.py
@@ -20,7 +20,6 @@ def get_things():
                 'icon': '',
                 'desc': ''
             },
-
             {
                 'id': 4,
                 'name': 'Thing4',
@@ -28,4 +27,37 @@ def get_things():
                 'desc': ''
             }
         ]
+    }
+def get_services(thing_id):
+    # if thing_id==1:
+    return {
+        'code' : 200,
+        'result' : [ {
+            'id': 1,
+            'thingId':thing_id,
+            'name': 'Service1',
+            'icon': '',
+            'desc': ''
+        },
+        {
+            'id': 2,
+            'thingId':thing_id,
+            'name': 'Service2',
+            'icon': '',
+            'desc': ''
+        },
+        {
+            'id': 3,
+            'thingId':thing_id,
+            'name': 'Service3',
+            'icon': '',
+            'desc': ''
+        },
+        {
+            'id': 4,
+            'thingId':thing_id,
+            'name': 'Service4',
+            'icon': '',
+            'desc': ''
+        },]
     }
